fe59402fde5169ef4db60e6eb8057a5dc7bfb0b5
Amir k <amir.k@dal.ca>
2025-08-03 23:49:51 -0400

    do not update the record if it already exists

--- app/helper/redditSentiments.py
+++ app/helper/redditSentiments.py
@@ -2,6 +2,7 @@ from app.models import RedditSentiments as RedditSentimentsModel
 from app.schemas.reddit_sentiments import RedditSentiment, RedditSentimentsCreate
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.dialects.postgresql import insert
 
 async def create_reddit_sentiments(session: AsyncSession, reddit_sentiments_create: list[RedditSentimentsCreate]):
     """
@@ -9,8 +10,10 @@ async def create_reddit_sentiments(session: AsyncSession, reddit_sentiments_crea
     """
     try:
         reddit_sentiments = [RedditSentimentsModel(**sentiment.model_dump()) for sentiment in reddit_sentiments_create]
-        session.add_all(reddit_sentiments)
-        # let the service handle the commit
+        records_to_insert = [sentiment.model_dump() for sentiment in reddit_sentiments_create]
+        stmt = insert(RedditSentimentsModel).values(records_to_insert)
+        stmt = stmt.on_conflict_do_nothing(index_elements=['post_id', 'comment_id'])
+        await session.execute(stmt)
         return reddit_sentiments
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error creating Reddit sentiments: {str(e)}; location i8HnRCqq2U")
+        raise HTTPException(status_code=500, detail=f"Error creating Reddit sentiments: {str(e)}; location i8HnRCO6Pw")
